f8d71e454e941b90299937b164a6fb99d27a4d33
Ben Smith <bh2smith@gmail.com>
2022-10-02 11:34:51 +0200

    export stuffs from index file

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,3 @@
-import { DuneClient } from "./client";
+export { DuneClient } from "./client";
 
-console.log("HELLO?");
-const client = new DuneClient("incorrect key");
-console.log(client);
-// console.log(await client.execute(1258228));
+export * from "./responseTypes";
