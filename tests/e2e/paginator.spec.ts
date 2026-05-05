bf292c2e8e5e5380f125f3d11abf019bf6148bde
Benjamin Smith <bh2smith@users.noreply.github.com>
2026-05-05 16:42:14 +0200

    Update devDependencies for security fixes (step 1) (#95)

--- tests/e2e/paginator.spec.ts
+++ tests/e2e/paginator.spec.ts
@@ -22,12 +22,12 @@ describe("Paginator Class", () => {
       number: 1,
       values: [
         {
-          hash: "0x88e96d4537bea4d9c05d12549907b32561d3bf31f45aae734cdc119f13406cb6",
-          number: 1,
+          hash: "0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3",
+          number: 0,
         },
         {
-          hash: "0xb495a1d7e6663152ae92708da4843337b958146015a2802f4193a410044698c9",
-          number: 2,
+          hash: "0x88e96d4537bea4d9c05d12549907b32561d3bf31f45aae734cdc119f13406cb6",
+          number: 1,
         },
       ],
     };
@@ -35,12 +35,12 @@ describe("Paginator Class", () => {
       number: 2,
       values: [
         {
-          hash: "0x3d6122660cc824376f11ee842f83addc3525e2dd6756b9bcf0affa6aa88cf741",
-          number: 3,
+          hash: "0xb495a1d7e6663152ae92708da4843337b958146015a2802f4193a410044698c9",
+          number: 2,
         },
         {
-          hash: "0x23adf5a3be0f5235b36941bcb29b62504278ec5b9cdfa277b992ba4a7a3cd3a2",
-          number: 4,
+          hash: "0x3d6122660cc824376f11ee842f83addc3525e2dd6756b9bcf0affa6aa88cf741",
+          number: 3,
         },
       ],
     };
@@ -53,8 +53,8 @@ describe("Paginator Class", () => {
       number: 5,
       values: [
         {
-          hash: "0x997e47bf4cac509c627753c06385ac866641ec6f883734ff7944411000dc576e",
-          number: 9,
+          hash: "0x2ce94342df186bab4165c268c43ab982d360c9474f429fec5565adfc5d1f258b",
+          number: 8,
         },
       ],
     };
